3a4eace7b13f2409dd33a20ea5d880cd2bcceb11
Oleg <forown@yandex.ru>
2016-04-21 09:33:23 +0300

    minor

--- addx.ml
+++ addx.ml
@@ -3,45 +3,12 @@ open Bap.Std
 open Asm.Reg
 open Addx_opcode
 
-let string_of_opcode op = 
-  Sexp.to_string (Addx_opcode.sexp_of_t64 op)
-
-(** [find_number_exn ~start str] - returns a number from string [str], 
-    starting at position [start]. Raise [Not_found] if no numbers found *)
-let find_number_exn ?(start=0) str =
-  let r = Str.regexp "[0-9]+[0-9]*" in
-  let _ = Str.search_forward r str start in
-  int_of_string (Str.matched_string str), Str.match_end ()
-
-(** [search_number_exn ~index op] - returns a number from [op]
-    name, with [index], e.g. `[search_number_exn ~index `ADD64ri32]`
-    will return 64 with index = 0 and 32 with index = 1. *)
-let search_number_exn ~index op =   
-  let s = string_of_opcode op in
-  let rec search cnt start =
-    let n,start' = find_number_exn ~start s in
-    if cnt = index then n
-    else search (cnt + 1) start' in
-  search 0 0
-
-let search_number_opt ~index op = 
-  try 
-    Some (search_number_exn ~index op)
-  with Not_found -> None
-
-let search_size_exn ~index op = 
-  Size.of_int_exn (search_number_exn ~index op)
-
-let search_size_opt ~index op = 
-  Option.(search_number_opt ~index op >>= fun w -> 
-          Size.of_int_opt w)
-
-let tmp name width = 
-  Var.create ~fresh:true ~is_virtual:true name (Type.Imm width)
-
 module Make (CPU : CPU) (Env : X86env.S) = struct
 
   open Env
+
+  let fresh name width = 
+    Var.create ~fresh:true ~is_virtual:true name (Type.Imm width)
 
   let msb r = Bil.(cast high 1 r)
   let set_nf res = Bil.(CPU.nf := msb res)
@@ -65,7 +32,7 @@ module Make (CPU : CPU) (Env : X86env.S) = struct
   let prepare_imm ~width ~value_width imm = 
     let imm = IM.of_imm imm in
     let exp = IM.get ~width:value_width imm in
-    let v0 = tmp "v0" (Size.in_bits width) in
+    let v0 = fresh "v0" (Size.in_bits width) in
     if width = value_width then Bil.move v0 exp, Bil.var v0
     else
       let exp' = Bil.(cast signed (Size.in_bits width) exp) in
@@ -103,7 +70,11 @@ module Make (CPU : CPU) (Env : X86env.S) = struct
 
   let add_ri_width op ops = 
     Operand.rri ops ~f:(fun dst src imm ->
-        let value_size = search_size_exn ~index:1 op in
+        let value_size = match op with
+          | `ADD32ri8 -> `r8
+          | `ADD16ri8 -> `r8
+          | `ADD64ri8 -> `r8
+          | `ADD64ri32 -> `r32 in
         add_ri_basic dst src imm value_size)   
 
   let add_rm op ops = 
@@ -133,13 +104,21 @@ module Make (CPU : CPU) (Env : X86env.S) = struct
 
   let add_mi op ops = 
     Operand.mi ops ~f:(fun mem imm ->
-        let width = search_size_exn ~index:0 op in
+        let width = 
+          match op with
+          | `ADD32mi -> `r32
+          | `ADD16mi -> `r16
+          | `ADD8mi -> `r8 in
         add_mi_basic mem imm width width)
 
   let add_mi_width op ops = 
     Operand.mi ops ~f:(fun mem imm ->
-        let width = search_size_exn ~index:0 op in
-        let value_width = search_size_exn ~index:1 op in
+        let width, value_width = 
+          match op with
+          | `ADD32mi8 -> `r32, `r8
+          | `ADD16mi8 -> `r16, `r8
+          | `ADD64mi8 -> `r64, `r8
+          | `ADD64mi32 -> `r64, `r32 in
         add_mi_basic mem imm width value_width)
 
   let add_rax op ops =
@@ -161,12 +140,12 @@ module Make (CPU : CPU) (Env : X86env.S) = struct
         match op with
         | #add_rr as op -> add_rr op ops
         | #add_ri as op -> add_ri op ops
-        | #add_ri_width as op -> add_ri_width op ops
         | #add_rm as op -> add_rm op ops
         | #add_mr as op -> add_mr op ops
         | #add_mi as op -> add_mi op ops
-        | #add_mi_width as op -> add_mi_width op ops
-        | #add_rax as op -> add_rax op ops)
+        | #add_rax as op -> add_rax op ops
+        | #add_ri_width as op -> add_ri_width op ops
+        | #add_mi_width as op -> add_mi_width op ops)
 end
 
 module Add64 = struct
